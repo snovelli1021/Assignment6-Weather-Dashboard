1ba6e3f597b5033cd81a70cb1ddff5c6b124e723
Stephen Novelli <snovelli1021@gmail.com>
2022-08-16 12:18:09 -0700

    Got city buttons to render on screen and weather data when a city is searched. Created classes and if/else/if to style uv index as low or high

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var searchBtn = document.getElementById("searchBtn");
-var cityList = document.querySelector('ul');
+
 
 //Setting up Local Storage
 function displayCity() {
@@ -15,16 +15,41 @@ searchBtn.addEventListener("click", function(event) {
 
 const searchAPI = function() {
     var city = document.querySelector("#inputCity").value;
-    var getURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=39ef46502d75ea57b3d7957787e6e636`;
+    var getURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=39ef46502d75ea57b3d7957787e6e636`;
     fetch(getURL)
         .then(function (response) {
             return response.json();
         }).then(function(responseObj){
             console.log(responseObj)
+            var lat = responseObj [0].lat
+            var lon = responseObj [0].lon
+            var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=39ef46502d75ea57b3d7957787e6e636&units=imperial`
+            fetch(weatherURL)
+            .then(function(response){
+                return response.json();
+            }).then(function(weatherData){
+                console.log(weatherData);
+                document.querySelector(".temp").innerText = weatherData.current.temp + " °"
+                document.querySelector(".humidity").innerText = weatherData.current.humidity + "%"
+                document.querySelector(".wind_speed").innerText = weatherData.current.wind_speed
+                document.querySelector(".uvi").innerText = weatherData.current.uvi
+                displayUVI(weatherData.current.uvi);
+                    
+            })
             saveCity();
         })     
 }
 
+function displayUVI(UVI) {
+    var uviEl = document.querySelector(".uvi")
+    uviEl.classList.remove("low" , "high")
+if (UVI < 2){
+  uviEl.classList.add("low")  
+} else if (UVI > 2) {
+    uviEl.classList.add("high") 
+}
+
+}
 
 const saveCity = function(){
     var cityInput = document.getElementById("inputCity").value;
@@ -41,10 +66,12 @@ const saveCity = function(){
 
 
 const renderCityList = function(cityList){
+    var cityListEl = document.querySelector('#cityList');
     for (let i = 0; i < cityList.length; i++) {
-        const element = array[index];
-        
+        console.log(cityList[i]);
+        var cityBtns = document.createElement("button")
+        cityBtns.innerText = cityList [i]
+        cityListEl.appendChild(cityBtns)
     }
-
 
 };
